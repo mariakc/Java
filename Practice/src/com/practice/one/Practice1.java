7d9201db72c7376fa13527fff1c65db80fb20fe1
hcui <huan.h.cui@ericssion.com>
2017-03-29 13:45:02 +0800

    upload to github test

---
+++ Practice/src/com/practice/one/Practice1.java
@@ -0,0 +1,17 @@
+package com.practice.one;
+
+public class Practice1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r = fun();
+		System.out.println("r = " + r);
+	}
+	
+	private static int fun(){
+		
+		return 3*5-8;
+		
+	}
+
+}
